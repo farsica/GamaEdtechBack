e95836e83066048100e08d81562f9a899803d794
farsica <fariborz2004@yahoo.com>
2025-07-30 20:18:02 +0330

    School Description

---
+++ src/Infrastructure/Infrastructure/Migrations/20250730164649_SchoolDescription.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GamaEdtech.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SchoolDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Schools",
+                type: "nvarchar",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Schools");
+        }
+    }
+}
